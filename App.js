d2422cbf3bbcda0940d9f7cfbc1e609a88e67f52
SantiagoReverter <santiago.reverter@insite-la.com>
2023-12-04 00:15:53 -0300

    lista actualizada

--- App.js
+++ App.js
@@ -1,16 +1,83 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, Modal, StyleSheet, Text, TextInput, TextInputComponent, View } from 'react-native';
+import uuid from 'react-native-uuid';
 
 export default function AppGym() {
+
+const [newProductTitle, setNewProductTitle] = useState("");
+const [products, setProducts] = useState([]);
+const [selectedProduct, setSelectedProduct] = useState({});
+const [modalVisible, setModalVisible] = useState(false);
+
+const handlerAddProduct = () =>{
+
+  const newProduct = {
+    id: uuid.v4(),
+    title: newProductTitle
+  }
+  setProducts(current => [...current, newProduct])
+  setNewProductTitle("");
+};
+
+const handlerDeleteProduct = () =>{
+  setProducts(current => current.filter(product => product.id ==! selectedProduct.id));
+  setModalVisible(false);
+}
+
+const handlerOpenModal = (item) =>{
+  setModalVisible(true);
+  setSelectedProduct({item});
+};
+
+const handlerCloseModal = () =>{
+  setModalVisible(false);
+};
+
   return (
     <View style={styles.container}>
       <Text>Hola, Coder!</Text>
-      <Button
-        onPress={apretame = () => {}}
-        title='Apretame'
-        color={"#000"}
-        accessibilityLabel='Que hara este boton?'
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder='completame'
+          onChangeText={(title) => setNewProductTitle(title)}
+          value = {newProductTitle}
+        />
+        <Button
+          onPress={() => handlerAddProduct()}
+          title='Agregar'
+          color={"#000"}
+        />
+      </View>
+      <FlatList
+        data={products}
+        keyExtractor={products.id}
+        renderItem={({item})=> <View
+                                  style={styles.textInput}
+                                >
+                                  <Text>Nombre de Producto: {item.title}</Text>
+                                  <Text>ID del producto: {item.id}</Text>
+                                  <Button
+                                    title='X'
+                                    onPress={() => handlerOpenModal(item)}
+                                  />
+                                </View>
+                    }
       />
+      <Modal
+        visible = {modalVisible} 
+      >
+        <Text>Desea eliminar este ejercicio?</Text>
+        <Button
+          title='Eliminar'
+          onPress={()=> handlerDeleteProduct()}
+        />
+        <Button
+          title='Cancelar'
+          onPress={()=> handlerCloseModal()}
+        />
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,6 +88,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'top',
+    marginTop: 50,
   },
+  inputContainer:{
+    flexDirection: "row",
+    width: "85%",
+  },
+  textInput: {
+    borderWidth: 3,
+    width: "75%",
+    marginRight: 10,
+    marginTop: 15,
+    padding: 10,
+  }
 });
